67d04f5985c4d44a4508bdc1a3460d47f626f9db
Kyra Cho <wsc2119@columbia.edu>
2024-11-25 12:47:27 -0800

    Update spinner.js

--- js/spinner.js
+++ js/spinner.js
@@ -1,7 +1,13 @@
 // funtion for the loading spinner
-// Replace spinner with iframe content once iframe is fully loaded
-const iframe = document.getElementById('iframe-content');
-  iframe.onload = () => {
-    document.getElementById('loading-icon').style.display = 'none';
-    iframe.style.display = 'block';
-  };
+// Replace spinner with iframe content once iframe is fully loaded, and once DOM is loaded. 
+document.addEventListener('DOMContentLoaded', () => {
+  const iframe = document.getElementById('iframe-content');
+  if (iframe) {
+    iframe.onload = () => {
+      document.getElementById('loading-icon').style.display = 'none';
+      iframe.style.display = 'block';
+    };
+  } else {
+    console.error('Iframe not found!');
+  }
+});
